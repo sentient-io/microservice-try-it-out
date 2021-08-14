e96b603672e7b0a42ce56fe74d0b0b2443e53d31
Zhang Qiao <zqthedesigner@gmail.com>
2021-08-14 19:48:26 +0800

    update

--- src/services/TryItOut/TryItOut_doc_service.ts
+++ src/services/TryItOut/TryItOut_doc_service.ts
@@ -3,7 +3,12 @@ import { docInterface } from './TryItOut_types';
 const tryItOutDocService = () => {
   function getImgBase64FromDocRef(docRef: docInterface) {
     console.log(docRef);
-    return '';
+    const inputProperties = docRef.components.schemas.input.properties;
+    if ('image_base64' in Object.keys(inputProperties)) {
+      return inputProperties['image_base64'].example;
+    } else {
+      return '';
+    }
   }
 
   return { getImgBase64FromDocRef };
