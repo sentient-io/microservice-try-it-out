08258212192405fbde618f65dff7d4fef87f7935
Zhang Qiao <zqthedesigner@gmail.com>
2021-09-11 21:44:13 +0800

    fix(not available for websocket): minor fix

--- src/components/TryItOut/TryItOut.vue
+++ src/components/TryItOut/TryItOut.vue
@@ -15,22 +15,24 @@
      -->
     <div
       v-show="Object.keys(rawDocRef).length === 0 || !rawDocRef.openapi"
-      class="row items-center justify-center q-my-lg"
+      class="row items-center justify-center q-my-lg no-wrap"
     >
       <div class="q-mr-md">
         <q-icon name="mdi-telescope" size="4rem" color="grey-4" />
       </div>
-      <div>
-        <p v-if="rawDocRef.openapi">
-          Oops, API documentation not found from the provided url below:
-        </p>
-        <p v-else>
-          Try it out is not available for websocket API at this moment.
-        </p>
-        <a :href="docPath" :target="isInIframe ? '_parent' : '_blank'">{{
-          docPath
-        }}</a>
+      <div v-if="rawDocRef.openapi">
+        <p>Oops, API documentation not found from the provided url below:</p>
+
+        <a
+          class="s-break-all text-green-8"
+          :href="docPath"
+          :target="isInIframe ? '_parent' : '_blank'"
+          >{{ docPath }}</a
+        >
       </div>
+      <h5 v-else class="text-grey-7">
+        Try It Out is currently not available for websocket APIs.
+      </h5>
     </div>
 
     <!-- 
