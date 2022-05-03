ba5667a8a1776d5c1983a27d4808d4ad9caa3457
Qiao Zhang <zqthedesigner@gmail.com>
2022-05-03 19:41:54 +0800

    update version number

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     <q-page-container class="full-width">
       <router-view style="max-width: 1200px" class="q-mx-auto" />
       <div class="text-left text-grey-5 q-pa-md">
-        <small>Try It Out Version: v0.3.0</small>
+        <small>Try It Out Version: v0.3.1</small>
       </div>
     </q-page-container>
   </q-layout>
